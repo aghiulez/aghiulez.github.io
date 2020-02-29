28a80ca2f78a4e8d2de486ece2f7a3795ce1f460
Ara <aghiulezian@gmail.com>
2020-02-28 23:17:31 -0800

    added some picture/gif to the experiences

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
 
+
+
 const Projects = ({user}) => {
+
+    let backgroundUrl = 'https://media-exp1.licdn.com/dms/image/C560BAQEjzxdA4RUnWQ/company-logo_200_200/0?e=1591228800&v=beta&t=i7BEuooI00FJhvV-W1lv-fr-COfwaSxqDMNA8_mtNhQ'
+
     return (
 
 
-        <section id="projects">
+        <section id="experience">
         <div className="flexbox">
             
             {user.work.map(work => (
-                <a href={work.website} target="_blank" key={work.company} > 
-                    <div className = "project" >
-                        <h1> {work.position} </h1>
-                        <p>{work.company}</p>
+                
+                <a href={work.website} className="link" target="_blank"  key={work.company}> 
+
+                    <h1 className='position'> {work.position} </h1>
+                    <div className = "project"  
+                    style={
+                        {
+                            background:   `url(${work.highlights[0]}) no-repeat`,
+                            backgroundSize: 'cover'
+                        }
+                            }
+                            >
+
+                        <div className='info'>
+                            {/* <h1> {work.position} </h1> */}
+                            {/* <p>{work.company}</p> */}
+                        </div>
+
                     </div>
                 </a>
             ))}
@@ -24,3 +43,6 @@ const Projects = ({user}) => {
 
 
 export default Projects;
+
+
+
