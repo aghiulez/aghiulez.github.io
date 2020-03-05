558a6fc2b0cefa0d387f16376716838a66c2536f
Ara <aghiulezian@gmail.com>
2020-03-05 15:43:36 -0800

    Updates

---
+++ precache-manifest.7be6a1263cc8349e73323ad1a5525873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64ac49b49e2221ff70f6a495e48224b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6c5e47ba22c9df1ba90",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "62fd07474ad46c1c1b64",
+    "url": "/static/js/2.7ddf9ba2.chunk.js"
+  },
+  {
+    "revision": "f7d55a45cfb1318df43c2a8ab76b9c67",
+    "url": "/static/js/2.7ddf9ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c5e47ba22c9df1ba90",
+    "url": "/static/js/main.2d43176e.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
