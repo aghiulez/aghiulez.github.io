e6c9f100e2d1f6290aa97920a2ca73710f9b4ce8
Ara <aghiulezian@gmail.com>
2022-06-23 19:02:37 -0700

    added responsive tailwind navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,60 +3,96 @@ import {
     Link
 } from "react-router-dom";
 
+// const pages = ['Products', 'Pricing', 'Login'];
+// const navLinks = pages.map(page => <a key={page} className="no-underline text-gray-800 font-semibold hover:text-gray-600" href={`#${page}`}>{page}</a>);
+const pages = ['about', 'projects', 'experience','contact'];
+const navLinks = pages.map(page => <Link to={'/'+ page} className="no-underline text-gray-800 font-semibold hover:text-gray-600"  aria-current="page">{page}</Link>);
+
+
+const Navbar = ({ menuOpen, setMenuOpen }) => (
+  <div className="flex items-center justify-between p-4">
+    <div className="flex items-center">
+      <FireSvg />
+      <a href="#home" className="text-xl font-bold no-underline text-gray-800 hover:text-gray-600">AraMatthew.com</a>
+    </div>
+    <nav className="hidden md:block space-x-6">
+      {navLinks}
+    </nav>
+    <button type="button" aria-label="Toggle mobile menu" onClick={() => setMenuOpen(!menuOpen)} className="rounded md:hidden focus:outline-none focus:ring focus:ring-gray-500 focus:ring-opacity-50"><MenuAlt4Svg menuOpen={menuOpen} /></button>
+  </div>
+);
+
+const MobileMenu = ({ children }) => (
+  <nav className="p-4 flex flex-col space-y-3 md:hidden">
+    {children}
+  </nav>
+);
+
+
+const FireSvg = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+    <path fillRule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clipRule="evenodd" />
+  </svg>
+);
+
+const MenuAlt4Svg = ({ menuOpen }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className={`transition duration-100 ease h-8 w-8 ${menuOpen ? 'transform rotate-90' : ''}`} viewBox="0 0 20 20" fill="currentColor">
+    <path fillRule="evenodd" d="M3 7a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 13a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+  </svg>
+);
+
 const Page = () => {
 
-    const [navOpen, setNavOpen] = useState(false);
+    // const [navOpen, setNavOpen] = useState(false);
+
 
     // return (
-    //    <section class="flex items-center justify-between flex-wrap bg-teal-500 p-6" id='nav'>
-    //     <nav class="flex items-center flex-shrink-0 text-white mr-6" className = {navOpen ? 'active' : ''}>
-
-
-    //             <div class="fill-current h-8 w-8 mr-2" className ="header">
-    //                 <Link class="font-semibold text-xl tracking-tight" className="AraMatthew" to="/">Ara<span className = 'boldBlue'>Matthew</span></Link>
-    //                 <figure className="dropbtn" onClick={ () => { setNavOpen(!navOpen) } }/>
+    //     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+    //         <div class="container flex flex-wrap justify-between items-center mx-auto">
+    //             <a href="https://flowbite.com/" class="flex items-center">
+    //                 {/* <img src="/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> */}
+    //                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">AraMatthew</span>
+    //             </a>
+    //             <div class="flex md:order-2">
+    //                 <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+    //                 <button data-collapse-toggle="mobile-menu-4" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-4" aria-expanded="false">
+    //                     <span class="sr-only">Open main menu</span>
+    //                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    //                     <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    //                 </button>
     //             </div>
+    //             <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-4">
+    //                 <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+    //                     <li>
+    //                         {/* <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a> */}
+    //                         <Link to="/about" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">About</Link>
 
-    //             <div onClick={ () => { setNavOpen(!navOpen) } } id='links' className = "links"> 
-    // <Link to="/about">About</Link>
-    // <Link to="/projects">Projects</Link>
-    // <Link to="/experience">Experience</Link>    
-    // <Link to="/contact">Contact</Link>               
+    //                     </li>
+    //                     <li>
+    //                         <Link to="/projects" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Projects</Link>
+    //                     </li>
+    //                     <li>
+    //                         <Link to="/experience" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Experience</Link>
+    //                     </li>
+    //                     <li>
+    //                         <Link to="/contact" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Contact</Link>
+    //                     </li>
+    //                 </ul>
     //             </div>
+    //         </div>
     //     </nav>
-    //     </section>
+    // )
 
-    // );
-
-
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
-        <nav class="flex items-center justify-between flex-wrap bg-gray-900 p-6">
-            <div class="flex items-center flex-shrink-0 text-white mr-6">
-                {/* <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg">
-
-                </svg> */}
-                <span class="font-semibold text-xl tracking-tight">AraMatthew</span>
-            </div>
-            <div class="block lg:hidden">
-                <button  class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"  >
-                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-                </button>
-            </div>
-            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto" >
-                <div class="text-sm lg:flex-grow"  id='links' className = "links">
-                    <Link to="/about" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">About</Link>
-                    <Link to="/projects" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Projects</Link>
-                    <Link to="/experience" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Experience</Link>    
-                    <Link to="/contact" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Contact</Link> 
-
-                </div>
-                {/* <div>
-                    <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
-                </div> */}
-            </div>
-        </nav>
-    )
-
+      <div className="bg-gradient-to-r from-blue-200 to-blue-100">
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        {menuOpen &&
+          <MobileMenu>
+            {navLinks}
+          </MobileMenu>}
+      </div>
+    );
 
 }
 
